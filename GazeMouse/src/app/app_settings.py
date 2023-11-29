7e30185eca8a0e82660d15ddb229e7a58a967562
Jacob Lopez <jacoblopez757@gmail.com>
2023-11-28 20:33:40 -0500

    add mouse actions

--- GazeMouse/src/app/app_settings.py
+++ GazeMouse/src/app/app_settings.py
@@ -1,4 +1,11 @@
+import mouse
+
 class app_settings():
     def __init__(self, screen_resolution = [2560,1440]):
         self.screen_resolution = screen_resolution
         self.actions = ['neutral', 'left-click', 'right-click', 'shutdown']
+        self.mouse_actions = [lambda : None, lambda : mouse.click(button='left'), mouse.click(button='right'), lambda : None]
+        self.user_to_mouse_actions = {user:mouse for user, mouse in zip(self.actions, self.mouse_actions)}
+    
+    def get_mouse_action(self, user_action):
+        return self.user_to_mouse_actions[user_action]
--- GazeMouse/src/scripts/face_actions_demo.py
+++ GazeMouse/src/scripts/face_actions_demo.py
@@ -9,6 +9,17 @@ from pathlib import Path
 
 relevant_categories = ['eyeBlinkLeft', 'eyeBlinkRight']
 
+def convert_detection_to_user_action(detection, history, metric):
+    detector_pred = detection.face_blendshapes[0]
+    detector_pred = [category for category in detector_pred if category.category_name in relevant_categories]
+    detector_pred = [category.score for category in detector_pred]
+    history = history[-1:]
+    history.append(detector_pred)
+    detector_pred = np.mean(history, axis=0)
+    print(detector_pred)
+    closest_action = metric.get_closest_user_action(detector_pred)
+    return closest_action
+
 def run():
     settings = app_settings()
     actions = np.array(settings.actions)
@@ -20,7 +31,7 @@ def run():
     webcam_capture = predictive_webcam_recorder(webcam_capture_queue, tracker_pred_queue, detector_pred_queue)
     multiprocessing.Process(target=webcam_capture.start_processing).start()
 
-    with open(str(Path('expressions.npy')), 'rb') as f:
+    with open(str(Path('GazeMouse/data/numpy/expressions.npy')), 'rb') as f:
         action_expressions = np.load(f)
     
     metric = expression_metric(actions, action_expressions)
@@ -28,14 +39,8 @@ def run():
     while True:
         captured_image = webcam_capture_queue.get()
         tracker_pred = tracker_pred_queue.get()
-        detector_pred = detector_pred_queue.get().face_blendshapes[0]
-        detector_pred = [category for category in detector_pred if category.category_name in relevant_categories]
-        detector_pred = [category.score for category in detector_pred]
-        history = history[-1:]
-        history.append(detector_pred)
-        detector_pred = np.mean(history, axis=0)
-        print(detector_pred)
-        closest_action = metric.get_closest_user_action(detector_pred)
+        detector_pred = detector_pred_queue.get()
+        closest_action = convert_detection_to_user_action(detector_pred, history, metric)
         print(closest_action)
 
 if __name__ == "__main__":
